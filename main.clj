1ab35d97db17b2756875ea66a76260b5e7ff5c93
Dan Whitaker <dan@uncharted.systems>
2022-02-19 22:37:02 -0700

    "initial terminal"

--- main.clj
+++ main.clj
@@ -1,11 +1,42 @@
 (ns main
   (:require [clojure.string :as string]))
 
-;; Use Puzzle Input for validation, store word strings as game state.
-;; [[4 P] [0 2] [1 N] [3 4] [0 F] [1 2] [3 C] [0 4] [2 A]]
-;; "sharP" "sNags" "Frags" "cloCk" "crAnk"
 ;; Use !*command* inputs to interact with the game
 ;; eg: !2 = enter row 2. !clear = clears board. !quit = quit game.
+
+;; Game Representation _ _ _ _ P
+;;    |   |
+;; >> _ N _ _ _
+;;          | |
+;;    F _ _ _ _
+;;      | |
+;;    _ _ _ C _
+;;    |       |
+;;    _ _ A _ _
+
+;; Internal representation of submitted words:
+;; {:1 "sharP"
+;;  :2 "sNags"
+;;  :3 "Flogs"
+;;  :4 "cloCk"
+;;  :5 "crAnk"}
+
+(def word-set (set (string/split-lines (slurp "words.txt"))))
+
+(defn validate [input words]
+  (contains? words input))
+
+(defn toy-puzzle [words]
+  (println "Type a word to test the list")
+  (println (validate (string/lower-case (read-line)) words)))
+
+
+
+
+;; Use Puzzle Input for validation and representation, store word strings as game state.
+;; [[4 P] [0 2] [1 N] [3 4] [0 F] [1 2] [3 C] [0 4] [2 A]]
+
+(def prototype [[4 "P"] [0 2] [1 "N"] [3 4] [0 "F"] [1 2] [3 "C"] [0 4] [2 "A"]])
 
 ;; Game Representation
 ;;    _ _ _ _ P
@@ -18,14 +49,33 @@
 ;;    |       |
 ;;    _ _ A _ _
 
-(def word-set (set (string/split-lines (slurp "words.txt"))))
+(defn convert [input]
+  (hash-map :rows (take-nth 2 input)
+            :links (keep-indexed #(if (odd? %1) %2) input)))
 
-(defn validate [input words]
-  (contains? words input))
+(defn convert-row [row empty]
+  ())
 
-(defn toy-puzzle [words]
-  (print "Type a word to test the list")
-  (flush)
-  (validate (string/lower-case (read-line)) words))
+(defn convert-link [link])
 
-(toy-puzzle word-set)
+(defn represent [board words]
+  (let [s ""
+        rows (:rows board)
+        links (:links board)
+        line-map {0 "_ "
+                  1 "_ "
+                  2 "_ "
+                  3 "_ "
+                  4 "_ "}]
+    (map convert-row rows)
+    (map convert-link links)
+    ))
+
+(defn puzzle [input valid-words]
+  (let [board (convert input)
+        words {}]
+    (represent board words)
+    (print board)
+    ))
+
+(puzzle prototype word-set)
