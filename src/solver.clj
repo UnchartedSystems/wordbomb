f021e0c984e2124aa06471eaf99fab44221db4ec
Daniel Whitaker <dan@uncharted.systems>
2022-08-30 15:19:27 -0600

    revisit repo & found profiler

--- src/solver.clj
+++ src/solver.clj
@@ -1,5 +1,23 @@
 (ns solver
-  (:require [utilities :as utils]))
+  (:require [utilities :as utils]
+            [taoensso.tufte :as tufte :refer (defnp p profiled profile)]))
+
+;; ;; We'll request to send `profile` stats to `println`:
+;; (tufte/add-basic-println-handler! {})
+
+;; ;;; Let's define a couple dummy fns to simulate doing some expensive work
+;; (defn get-x [] (Thread/sleep 500)             "x val")
+;; (defn get-y [] (Thread/sleep (rand-int 1000)) "y val")
+
+;; ;; How do these fns perform? Let's check:
+
+;; (profile ; Profile any `p` forms called during body execution
+;;   {} ; Profiling options; we'll use the defaults for now
+;;   (dotimes [_ 5]
+;;     (p :get-x (get-x))
+;;     (p :get-y (get-y))))
+
+
 
 ;; NOTE: A fitness function that values curated words more then all words would be useful
 ;; HACK REVIEW TODO: Rewrite Solution Search to not blow the stack and allow concurrency
