fe3d2c239543f1206b06e5baec2ca56c6f4f6bb4
Uncharted Systems <dan@uncharted.systems>
2023-02-19 05:29:59 -0700

    total solver overhaul

--- src/solver2.clj
+++ src/solver2.clj
@@ -5,22 +5,10 @@
 ;; We'll request to send `profile` stats to `println`:
 (tufte/add-basic-println-handler! {})
 
-;;; Let's define a couple dummy fns to simulate doing some expensive work
-(defn get-x [] (Thread/sleep 500)             "x val")
-(defn get-y [] (Thread/sleep (rand-int 1000)) "y val")
-
-;; How do these fns perform? Let's check:
-
-(profile ; Profile any `p` forms called during body execution
-  {} ; Profiling options; we'll use the defaults for now
-  (dotimes [_ 5]
-    (p :get-x (get-x))
-    (p :get-y (get-y))))
-
-
 ;; TODO:
 ;;  Take in wordset & puzzle
 ;;  PARALLELIZE: Get row-compatible subset per row
+;;    NOTE: nope, overhead too high
 ;;  EITHER:
 ;;    PARALLELIZE:  per row-subset, get next-row-compatible subset per word
 ;;    OR
@@ -29,3 +17,43 @@
 ;;                  in the next row, eliminate it from the subset for selection
 ;;                  for the same process in the previous row
 ;;  Get all possible row combinations from resulting data structure and clean it up
+
+
+
+(def input [[4 \P] [2 0] [1 \N] [3 4] [0 \F] [1 2] [3 \C]])
+
+; [4 \P]
+
+(defn- get-row-subset [row-constraint wordset]
+  (let [compatible? (fn [word] (= (get word (first row-constraint))
+                                  (second row-constraint)))]
+    (filter compatible? wordset)))
+
+(def mock-rows [[4 \P] [1 \N] [0 \F] [3 \C]])
+
+(defn- get-all-row-subsets [rows-constraints wordset]
+  (mapv #(vec (get-row-subset % wordset)) rows-constraints))
+
+(def mock-next-row [2 0])
+
+;FIXME: rename
+(defn filter-to-linked-words [link-fns word next-row]
+  (let [compatible? (fn [next-word] (apply = true (map #(%1 %2 %3) link-fns word next-word)))]
+    (filter compatible? next-row)))
+
+; FIXME: rename
+; TODO: save new subset for next base-row & save base-words -> next-words
+(defn- x-setup [link-constraint base-row next-row]
+  (let [link-bools (mapv #(contains? (set link-constraint) %) (range 5))
+        link-fns (mapv #(if % = not=) link-bools)]
+    (mapv #(filter-to-linked-words link-fns % next-row) base-row)))
+
+(x-setup [2 0]
+         (first (get-all-row-subsets mock-rows utils/core-words))
+         (second (get-all-row-subsets mock-rows utils/core-words)))
+
+#_(profile
+ {}
+ (dotimes [_ 5]
+   (p :l (get-all-row-subsets mock-input utils/core-words))
+   (p :v (get-all-row-subsetsv mock-input utils/core-words))))
