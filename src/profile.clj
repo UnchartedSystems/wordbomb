0280653085b78294da8d9428db34940c03d85ce2
Uncharted Systems <dan@uncharted.systems>
2023-02-28 19:57:58 -0700

    fast linksets with transducers and transients! -ed

--- src/profile.clj
+++ src/profile.clj
@@ -25,6 +25,9 @@
          (p :flat (flatten '("1" (1 2 3 4 (1 2 3 4 5 6 7 8 9 10) 5 6 7 (1 2 3 4 5 6 7 8 9 10) 8 9 10) 2 3 4 (1 2 3 4 (1 2 3 4 (1 2 3 4 5 6 7 8 9 10) 5 6 7 (1 2 3 4 5 6 7 8 9 10) 8 9 10) 5 6 7 8 9 10) 5 6 7 (1 2 3 4 5 6 7 8 9 10) 8 9 10)))
          (p :apply (apply = true '(true true true true false false true true)))
          (p :every? (every? true? '(true true true true false false true true)))
+         (p :jva (.charAt "HELLO" 3))
+         (p :get (get "HELLO" 3))
+         (p :nth (nth "HELLO" 3))
          (p :reduce (reduce + 0 '(1 2 3 4 5 6 7 8 9 10)))))
 
 
@@ -32,6 +35,13 @@
 (profile
  {}
  (let [rows    (take-nth 2 input)]
-   (dotimes [_ 10]
-     (p :s4)(s4/linksets input utils/all-words)
+   (dotimes [_ 3]
+     (p :4d (s4/linksets4 input utils/all-words))
+     (p :4c (s4/linksets3 input utils/all-words))
+     (p :4b (s4/linksets2 input utils/all-words))
+     (p :4a (s4/linksets input utils/all-words))
+     ;; (p :ks (keep seq '(() (1) (1 2) () (3))))
+     ;; (p :ke (keep not-empty '(() (1) (1 2) () (3))))
+     ;; (p :fe (filter not-empty '(() (1) (1 2) () (3))))
+     ;; (p :fv (filterv not-empty '(() (1) (1 2) () (3))))
      )))
